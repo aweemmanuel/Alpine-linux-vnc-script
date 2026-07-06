b12bd6b71fac027034620d29a8614c58b3bb0c78
aweemmanuel <117767329+aweemmanuel@users.noreply.github.com>
2026-07-06 02:35:56 +0100

    Fix formatting and simplify x11vnc and websockify commands

--- start.sh
+++ start.sh
@@ -3,20 +3,12 @@
 export DISPLAY=:99
 
 Xvfb :99 -screen 0 1366x768x24 &
-
 sleep 2
 
 openbox &
 
-x11vnc \
--display :99 \
--forever \
--nopw \
--shared &
+x11vnc -display :99 -forever -nopw -shared &
 
 firefox &
 
-websockify \
---web=/usr/share/novnc \
-6080 \
-localhost:5900
+websockify --web=/usr/share/novnc 6080 localhost:5900
